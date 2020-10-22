c6d58ce978234f7b9a77abaf13d804feecc5a578
JCHappytime <18782959613@163.com>
2020-10-22 22:48:56 +0800

    修改主页整体效果

---
+++ src/views/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="footer">
+      <div class="content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      user: {},
+    }
+  },
+  created() {},
+  beforeCreate() {},
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  height: 1700px;
+  background-color: black;
+}
+</style>
