74478bde251a7b6de21dbbc8da187b59b313d544
JCHappytime <18782959613@163.com>
2020-12-07 00:31:30 +0800

    下拉框实现

---
+++ src/components/TreeSelect.vue
@@ -0,0 +1,240 @@
+<template>
+  <div>
+    <div>多级树形选择框</div>
+    <!-- <select class="select">
+      <option value="1">text1</option>
+      <option value="2">text1</option>
+      <option value="3">text1</option>
+      <option value="4">text1</option>
+      <option value="5">text1</option>
+      <option value="6">text1</option>
+    </select> -->
+    <!-- <ul style="width: 335px;">
+      <li>parent-1
+        <ul>
+          <li>child1.1</li>
+          <li>child1.2</li>
+        </ul>
+      </li>
+      <li>parent-2
+        <ul>
+          <li>child2.1</li>
+          <li>child2.2</li>
+        </ul>
+      </li>
+    </ul> -->
+
+    <!-- <select class="select">
+      <optgroup label="a">
+          <option>a1</option>
+          <option>a2</option>
+          <option>a3</option>
+      </optgroup>
+      <hr class="hr-wrap" style="padding: 10px 0;">
+      <optgroup label="b">
+          <option>b1</option>
+          <option>b2</option>
+          <option>b3</option>
+      </optgroup>
+  </select> -->
+
+  <!-- <select class="select">
+    <optgroup label="Java">
+        <option value="jsp Servlet">jsp Servlet</option>
+        <option value="javaEE">javaEE</option>
+    </optgroup>
+    <optgroup label=".Net">
+        <option value="aspx">aspx</option>
+        <option value="C#">C#</option>
+    </optgroup>
+</select> -->
+
+<!-- <div class="contact">
+  <a class="select"></a>
+  <div class="options">
+    <div class="contact-wrap">
+      <a>向销售部咨询（后面可以加图标）</a>
+      <a>线上咨询（后面可以加图标）</a>
+      <a>常见问题（后面可以加图标）</a>
+    </div>
+  </div>
+</div> -->
+
+<div data-v-52fb32a6="" dropdown="" class="dropdown for-calc">
+  <a class="dropdown-btn btn open">
+    <span data-v-52fb32a6="">KRW</span>
+    <i data-v-1c95b306="" data-v-52fb32a6="" icon="" class="ml-5 chev-down"></i>
+    <i data-v-1c95b306="" data-v-52fb32a6="" icon="" class="ml-5 chev-up"></i>
+  </a><!---->
+  <div class="dropdown-menu" style="top: 49.2708px; left: 247px; width: auto;"><p data-v-52fb32a6="" class="dropdown-title">常用汇率</p><ul data-v-52fb32a6="" class="dropdown-content multi-line"><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    KRW
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="KRW"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    USD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="USD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    JPY
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="JPY"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    EUR
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="EUR"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    HKD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="HKD"><i data-v-50be8890=""></i></label></li></ul>
+    <hr class="mv-10">
+    <ul data-v-52fb32a6="" class="dropdown-content multi-line"><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    AED
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="AED"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    AUD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="AUD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    CHF
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="CHF"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    CNY
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="CNY"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    COP
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="COP"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    EUR
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="EUR"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    GBP
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="GBP"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    HKD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="HKD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    IDR
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="IDR"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    JPY
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="JPY"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    KRW
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="KRW"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    MYR
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="MYR"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    SGD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="SGD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    THB
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="THB"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    TRY
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="TRY"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    TWD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="TWD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    USD
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="USD"><i data-v-50be8890=""></i></label></li><li data-v-52fb32a6="" class="dropdown-item"><label data-v-50be8890="" data-v-52fb32a6="" check="">
+    VND
+    <input data-v-52fb32a6="" data-v-50be8890="" type="radio" value="VND"><i data-v-50be8890=""></i></label></li></ul></div>
+</div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+/* .select {
+  width: 335px;
+  height: 36px;
+  margin: 20px 0 30px 0;
+  border-radius: 4px;
+} */
+li {
+  list-style-type: none;
+}
+.hr-wrap {
+  margin: 30px 0;
+  height: 1px;
+  border: none;
+  background-color: #888888;
+}
+.contact {
+  position: relative;
+}
+.select{
+  display: inline-block;
+    font-size: .8125rem;
+    line-height: 1.5;
+    line-height: 28px;
+    font-weight: 500;
+    border: 1px solid #1e232e;
+    border-radius: 2px;
+    background-color: #fff;
+    height: 30px;
+    color: #1e232e;
+    margin: 16px 0 0;
+    cursor: pointer;
+    text-align: center;
+    /* -webkit-box-sizing: border-box; */
+    box-sizing: border-box;
+}
+.options {
+  position: absolute;
+    right: 0;
+    top: 45px;
+    text-align: left;
+}
+.contact-wrap {
+  margin-top: 5px;
+    background: #fff;
+    box-shadow: 1px 4px 16px 0 rgba(0,0,0,.24), 0 1px 4px 0 rgba(0,0,0,.1);
+    width: 198px;
+    height: 209px;
+    padding: 12px 0;
+    box-sizing: border-box;
+}
+
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  padding: 0!important;
+}
+.dropdown.for-calc .dropdown-btn {
+  display: block;
+  text-align: left;
+  padding: 8px 12px;
+}
+.dropdown .dropdown-btn {
+  font-size: .875rem;
+    letter-spacing: -.42px;
+    line-height: 1.5;
+    line-height: 1;
+    font-weight: 500;
+    letter-spacing: -.25px;
+    padding: 8px 19px;
+    border: 1px solid #222;
+    border-radius: 20px;
+    color: #fff;
+    background-color: #222;
+}
+.dropdown .dropdown-btn.open {
+    color: #fff;
+    border-color: #14adea;
+    background-color: #14adea;
+}
+.dropdown .dropdown-menu {
+    position: fixed;
+    border: 1px solid #dadada;
+    margin-top: 10px;
+    z-index: 998;
+    padding: 12px 18px;
+    background: #fff;
+    border-radius: 4px;
+    min-width: 110px;
+    letter-spacing: 0;
+    top: 49.2708px;
+    left: 247px;
+    width: auto;
+}
+.dropdown .dropdown-menu .dropdown-title {
+    color: #222;
+    margin: 0 15px 10px 0;
+    font-size: 1rem;
+    letter-spacing: -.48px;
+    line-height: 1.5;
+    line-height: 1.375;
+    font-weight: 500;
+}
+.dropdown.for-calc .dropdown-menu .dropdown-content.multi-line {
+    float: left;
+    width: 400px;
+}
+.dropdown.for-calc .dropdown-menu .dropdown-content.multi-line .dropdown-item {
+    float: left;
+    width: 40%;
+    margin-right: 16px;
+}
+.mv-10 {
+    margin-top: 10px!important;
+    margin-bottom: 10px!important;
+}
+</style>
